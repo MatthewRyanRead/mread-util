6b7731a7dd963e13c4acdda8cde8d30ba6f7dfe4
Matthew Read <MatthewRyanRead@users.noreply.github.com>
2016-09-23 14:25:17 -0400

    Initial commit with basic helpers

---
+++ mread-util.sh
@@ -0,0 +1,74 @@
+gripeb() {
+    grep --color=always -rIE --exclude-dir=\.git "$@" . | less -r ;
+}
+
+alias gripe='gripeb 2>/dev/null'
+alias igripe='gripe -i'
+
+jgripe() {
+    local _IGNORECASE=""
+    if [ "$1" == "-i" ]
+    then
+        _IGNORECASE="-i" ;
+        shift
+    fi
+
+    if [ "$1" != "-j" ]
+    then
+        gripe $_IGNORECASE --include \*.java "$@"
+    else
+        _FNAME="$2" ;
+        shift 2 ;
+        gripe $_IGNORECASE --include "$_FNAME".java "$@"
+    fi
+}
+
+alias jigripe='jgripe -i'
+
+testgripe() {
+    grep -rIE "(Failure|Error)s: [^0]" "$@" | while read -r line ; do
+        cat $(echo $line | awk -F':' '{print $1}') | less
+    done
+}
+
+addcert() {
+    certutil -d sql:$HOME/.pki/nssdb -A -t P -n $1 -i $1
+}
+
+rebase() {
+    local i='-i'
+    if [ "$1" == "+i" ]; then i=''; fi
+    git fetch && git rebase "${@:2}" $i origin/master
+}
+
+gitcrunch() {
+    git add -u &&
+    git commit --amend --no-edit &&
+    git fetch &&
+    git rebase origin/master &&
+    git push -f
+}
+
+alias changedfiles='git diff --name-only HEAD~1'
+
+killname() {
+    sudo ps aux | grep -i "$1" | grep -v grep | awk '{print $2}' | while read line; do sudo kill -9 $line; done
+}
+
+bashedit() {
+    vi ~/.bash_profile
+    source ~/.bash_profile
+}
+
+utiledit() {
+    vi ~/Developer/mread-util/mread-util.sh
+    source ~/Developer/mread-util/mread-util.sh
+}
+
+alias cls='clear'
+
+github_create_repo() {
+    curl -u "$GITHUB_USERNAME" https://api.github.com/user/repos -d '{"name":"'"$1"'"}' -H 'X-GitHub-OTP: '"$2"
+}
+alias ghcr='github_create_repo'
+
